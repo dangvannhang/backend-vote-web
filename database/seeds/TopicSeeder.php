6f20c92f9903c973933c810ad9e71e5b124e2e04
dangvannhang <dangvannhang107@gmail.com>
2021-05-15 22:10:43 +0700

    Edit TopicSeeder.

--- database/seeds/TopicSeeder.php
+++ database/seeds/TopicSeeder.php
@@ -17,25 +17,25 @@ class TopicSeeder extends Seeder
         DB::table('topic')->insert([
             [
                 'title' => 'Food',
-                'image'=> 'https://www.dietdoctor.com/wp-content/uploads/2019/03/Low-carb-food-200x200.jpg',
+                'image'=> 'https://s3-eu-west-1.amazonaws.com/uploads.playbaamboozle.com/uploads/images/152185/1607591595_198751',
                 'description' => 'Most Popular Food in the World'
             ],
 
             [
                 'title' => 'Travel',
-                'image' => 'https://lh3.googleusercontent.com/proxy/Zn9fimZEEtfK2CAN8AUywfQbCmUmVnl8PaaZd4O2Iu4vJSTSTbI7CYnYy6x8rYkNf0E2WnUtdUtcmAEnR1rSl30fmI7kabunuo2PqKNGejXa1naYBoiNb6-2Y9Pg-ULsCYA',
+                'image' => 'https://amilawfirm.com/wp-content/uploads/2019/05/auto-draft-3-6.jpg',
                 'description' => "World's Best Places to Visit"
             ],
 
             [
                 'title' => 'Online Games',
-                'image' => 'https://lh3.googleusercontent.com/proxy/7g-2hND440mwDYDTQK636Wnkt5MzfD9FAlSmcgNrwI93743Q2JPMMWUrGpkeim3BvAp7zKPL4BgcRYSngqPqoDx5_D8MP81pmlItNHdLbPgf',
+                'image' => 'https://cdn.tgdd.vn//GameApp/1305640//PicsArt11-10-02.08.50-800x450.jpg',
                 'description' => 'The Most Popular Online Games in the USA'
             ],
 
             [
                 'title' => 'Social Media',
-                'image' => 'https://libraryeuroparl.files.wordpress.com/2014/02/social-media.jpg?w=200',
+                'image' => 'https://caodang.fpt.edu.vn/wp-content/uploads/Social-Media-History.jpg',
                 'description' => 'The most popular social media sites'
             ]
         ]);
