bc090cae8526fb43e40e590347e5fef3712d2c1f
dangvannhang <dangvannhang107@gmail.com>
2021-05-17 01:00:41 +0700

    foreach data from web and handle user status

--- app/Http/Controllers/Api/UserStatusController.php
+++ app/Http/Controllers/Api/UserStatusController.php
@@ -21,7 +21,7 @@ class UserStatusController extends Controller
     public function addUserStatus(Request $request) {
 
         $id_user = $request->id_user;
-        $id_option = $request->id_option;
+        $id_options = $request->id_option;
 
         // $user_status = new UserStatus;
         // $user_status -> id_user = $id_user;
@@ -29,8 +29,21 @@ class UserStatusController extends Controller
         // $user_status -> save();
 
         // return response()->json($user_status,201);
+        
 
-        return response()->json($id_option);
+        // hien tai id_options la mot array
+        foreach($id_options as $id_option) {
+            $user_status = new UserStatus;
+            $user_status -> id_user = $id_user;
+            $user_status -> id_option = $id_option;
+            $user_status -> save();
+        }
+
+
+
+        // return response()->json($id_option);
+
+        return $this->getStatusUser($id_user);
     }
 
 }
